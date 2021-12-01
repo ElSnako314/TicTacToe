c76125b4fe000cbf8218672fb1a53b84358fb2e8
ElSnako314 <802913@sylvaniastudents.org>
2021-12-01 14:23:30 -0500

    Update Board.js

--- client/src/Board.js
+++ client/src/Board.js
@@ -49,24 +49,50 @@ function Board() {
             isClicked: false
         },
     ])
-    const [boardText, setBoardText] = useState("")
-    const [boardClicked, setBoardClicked] = useState(false)
     
+
     const turn = (id) => {
-        const index = boardBoxes.findIndex( (boardBox) => boardBox.id == id)
+        const index = boardBoxes.findIndex( (boardBox) => boardBox.id === id)
         if (!boardBoxes[index].isClicked) boardBoxes[index].text = "X"
         boardBoxes[index].isClicked = true
         setBoardBoxes([...boardBoxes])
+
+        const b = [];
+        for (let i = 0; i < boardBoxes.length; i++) {
+            b[i] = boardBoxes[i].text
+        }
+
+        console.log(checkwin(b.flat()))
+        if (checkwin(b.flat())) console.log("YAS")
     }
 
-    const checkwin = () => {
+    function checkwin(boardInLine) {
+        const [b1, b2, b3, b4, b5, b6, b7, b8, b9] = boardInLine
 
+        return (
+            checkThreeBoxes(b1, b2, b3) ||
+            checkThreeBoxes(b4, b5, b6) ||
+            checkThreeBoxes(b7, b8, b9) ||
+            checkThreeBoxes(b1, b4, b7) ||
+            checkThreeBoxes(b2, b5, b8) ||
+            checkThreeBoxes(b3, b6, b9) ||
+            checkThreeBoxes(b1, b5, b9) ||
+            checkThreeBoxes(b3, b5, b7)
+        )
+    }
+
+    function checkThreeBoxes(ti, ta, to) {
+        if (!(ti === "") || !(ta === "") || !(to === "")) {
+            return ti === ta && ta === to
+        }
+        else return false
     }
 
     return(
-        <div class="board-square">
+        <div className="board-square">
             {boardBoxes.map((boardBox) => 
                 <BoardSquare
+                    key={boardBox.id}
                     id={boardBox.id}
                     text={boardBox.text}
                     isClicked={boardBox.isClicked}
